be438cb7ba8ab142e7590f9d67e161ee7149d4ff
akil2309 <77388405+akil2309@users.noreply.github.com>
2021-02-14 08:40:20 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,74 @@
-import random
-attempts_list = []
-def show_score():
-    if len(attempts_list) <= 0:
-        print("there is currently no high score, its yours for the taking")
-    else:
-        print("the current high score is {} attempts".format(min(attempts_list)))
-def start_game():
-    random_number = int(random.randint(1,10))
-    print("hello traveller! welcome to the game of guesses!")
-    player_name = input("what is your name?")
-    wanna_play = input("hi {} would you like to play the game (enter y/n)".format(player_name))
-    attempts = 0
-    show_score()
-    while wanna_play.lower() == "y" :
-        try:
-            guess = input("pick a number between 1 to 10")
-            if int(guess)<1 or int(guess) > 10:
-                raise ValueError("guess the number within the range")
-            if int(guess) == random_number:
-                print("nice you got it!")
-                attempts += 1
-                attempts_list.append(attempts)
-                print("It took you {} attempts".format(attempts))
-                play_again = input("Would you like to play again? (Enter Yes/No) ")
-                attempts = 0
-                show_score()
-                random_number = int(random.randint(1, 10))
-                if play_again.lower() == "no":
-                    print("That's cool, have a good one!")
-                    break
-            elif int(guess) > random_number:
-                print("It's lower")
-                attempts += 1
-            elif int(guess) < random_number:
-                print("It's higher")
-                attempts += 1
-        except ValueError as err:
-                print("Oh no!, that is not a valid value. Try again...")
-                print("({})".format(err))
-    else:
-        print("That's cool, have a good one!")
+# madlib program
+# import modules
+from tkinter import *
 
-if __name__ == '__main__':
-   start_game()
+# creating a display window
+root = Tk()
+root. geometry('300x300')
+root.title('Mad Libs Generator')
+Label(root, text='Mad Libs Generator \n lets take the fun', font='arial 20 bold').pack()
+Label(root, text='Click Any One :', font='arial 15 bold').place(x=40, y=80)
+
+# function definition
+def madlib1():
+    animals = input('enter a animal name : ')
+    profession = input('enter a profession name: ')
+    cloth = input('enter a piece of cloth name: ')
+    things = input('enter a thing name: ')
+    name = input('enter a name: ')
+    place = input('enter a place name: ')
+    verb = input('enter a verb in ing form: ')
+    food = input('food name: ')
+    print(
+        'say ' + food + ', the photographer said as the camera flashed! ' + name + ' and,'
+        ' I had gone to ' + place + ' to get our photos taken on my birthday.,'
+        'The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending,'
+        ' to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We,'
+        ' both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
+
+
+def madlib2():
+    adjective = input('enter adjective : ')
+    color = input('enter a color name : ')
+    thing = input('enter a thing name :')
+    place = input('enter a place name : ')
+    person = input('enter a person name : ')
+    adjective1 = input('enter a adjective : ')
+    insect = input('enter a insect name : ')
+    food = input('enter a food name : ')
+    verb = input('enter a verb name : ')
+
+    print(
+        'Last night I dreamed I was a ' + adjective + ' butterfly with ' + color + ' splocthes,'
+        ' that looked like ' + thing + ' .I flew to ' + place + ' with my best-friend,'
+        ' and ' + person + ' who was a ' + adjective1 + ' ' + insect + ' .We ate,'
+        ' some ' + food + ' when we got there and then decided to ' + verb + ' and the dream,'
+        ' ended when I said-- lets ' + verb + '.')
+
+
+def madlib3():
+    person = input('enter person name: ')
+    color = input('enter color : ')
+    foods = input('enter food name : ')
+    adjective = input('enter aa adjective name: ')
+    thing = input('enter a thing name : ')
+    place = input('enter place : ')
+    verb = input('enter verb : ')
+    adverb = input('enter adverb : ')
+    food = input('enter food name: ')
+    things = input('enter a thing name : ')
+
+    print(
+        'Today we picked apple from ' + person + "'s Orchard. I had no idea there were so many,"
+        " different varieties of apples. I ate " + color + ' apples straight off,'
+        ' the tree that tested like ' + foods + '. Then there was a ' + adjective + ' apple,'
+        ' that looked like a ' + thing + '.When our bag were full, we went on a free,'
+        ' hay ride to ' + place + ' and back. It ended at a hay pile where we got,'
+        ' to ' + verb + ' ' + adverb + '. I can hardly wait to get home and cook with the,'
+        ' apples. We are going to make apple ' + food + ' and ' + things + ' pies!.')
+# Buttons
+    Button(root, text='The Photographer', font='arial 15', command=madlib1, bg='ghost white').place(x=60, y=120)
+    Button(root, text='apple and apple', font='arial 15', command=madlib3, bg='ghost white').place(x=70, y=180)
+    Button(root, text='The Butterfly', font='arial 15', command=madlib2, bg='ghost white').place(x=80, y=240)
+
+    root.mainloop()
